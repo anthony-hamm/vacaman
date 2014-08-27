e138c9af75169ccc5f27b3570fbfaef90110c3b0
anthony <anthony@anthony-VirtualBox.(none)>
2014-08-27 10:01:44 -0600

    RoR project creation and Angular setup.

---
+++ vacaman_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VacamanApp::Application.config.secret_key_base = '236847764eff9b1c93987629d6d07b6a0fd7efe50a4d856e0583618997d83ec09eabd2ca0514014d6955605a9ffba47227803d09cc6ce2b4d0e1656ce00fe73d'
---
+++ vacaman_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VacamanApp::Application.config.session_store :cookie_store, key: '_vacaman_app_session'
